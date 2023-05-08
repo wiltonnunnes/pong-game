33d857b7f5a73d91d5d696406e01980dd4a7ff58
wiltonnunnes <will_nunnes@hotmail.com>
2023-05-07 22:24:25 -0300

    Commit 07/05/2023 22:24

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const http = require('http');
-let fs = require('fs');
+import { WebSocketServer } from "ws";
+import http from 'http';
+import fs from 'fs';
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -27,6 +28,101 @@ const server = http.createServer((req, res) => {
 
     }
 });
+
+const speed = 20;
+
+let socket1 = null;
+let socket2 = null;
+
+const courtDimension = [864, 600];
+
+const paddle1 = {
+    x: 64,
+    y: courtDimension[1] / 2 - 64,
+    w: 32,
+    h: 128
+};
+
+const paddle2 = {
+    x: courtDimension[0] - 96,
+    y: courtDimension[1] / 2 - 64,
+    w: 32,
+    h: 128
+};
+
+const ball = {
+    x: courtDimension[0] / 2 - 16,
+    y: courtDimension[1] / 2 - 16,
+    w: 32,
+    h: 32,
+    v: [-1, 0]
+};
+
+const createState = () => {
+    return {
+        player1YPos: paddle1.y,
+        player2YPos: paddle2.y,
+        ballPos: [ball.x, ball.y]
+    };
+};
+
+const detectCollision = (x1, w1, x2, w2) => {
+    return (x1 <= x2 + w2 && x1 + w1 >= x2);
+};
+
+const clamp = (value, min, max) => {
+    if (value < min)
+        return min;
+    else if (value > max)
+        return max;
+    else
+        return value;
+};
+
+const webSocketServer = new WebSocketServer({ port: 8080 });
+
+webSocketServer.on('connection', (socket) => {
+    let paddle;
+    if (socket1 === null) {
+        socket1 = socket;
+        socket.send(JSON.stringify(createState()));
+        paddle = paddle1;
+    } else {
+        socket2 = socket;
+        socket.send(JSON.stringify(createState()));
+        socket1.send(JSON.stringify(createState()));
+        paddle = paddle2;
+    }
+
+    socket.on('message', (msg) => {
+        paddle.y = clamp(paddle.y + parseInt(msg) * speed, 0, courtDimension[1] - paddle.h);
+        socket1.send(JSON.stringify(createState()));
+        if (socket2 !== null)
+            socket2.send(JSON.stringify(createState()));
+    });
+});
+
+let frame = 10000 / 60;
+let loop = () => {
+    if (detectCollision(ball.x, ball.w, paddle1.x, paddle1.w)) {
+        ball.v[0] *= -1;
+        ball.v[1] *= ball.y + ball.h / 2 - paddle1.y + paddle1.h / 2;
+    }
+    if (detectCollision(ball.x, ball.w, paddle2.x, paddle2.w)) {
+        ball.v[0] *= -1;
+        ball.v[1] *= (ball.y + ball.h / 2) - (paddle2.y + paddle2.h / 2);
+    }
+
+    ball.x += ball.v[0] * speed;
+    ball.y += ball.v[1] * speed;
+    if (socket1 !== null) {
+        socket1.send(JSON.stringify(createState()));
+    }
+    if (socket2 !== null)
+        socket2.send(JSON.stringify(createState()));
+    setTimeout(loop, frame);
+};
+setTimeout(loop, frame);
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
--- public/js/pong.js
+++ public/js/pong.js
@@ -1,6 +1,71 @@
+const connect = (callback) => {
+    const socket = new WebSocket('ws://localhost:8080');
+    socket.addEventListener('open', (event) => {
+        callback(socket);
+    });
+};
+
+let isConnected = false;
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let nick = document.getElementById('nick');
+    connect((socket) => {
+        isConnected = true;
+        socket.send(nick.value);
+    });
+
+    nick.value = "";
+};
+
+const canvasWidth = 864;
+const canvasHeight = 600;
+
+const gameState = {
+    player1YPos: canvasWidth / 2 - 64,
+    player2YPos: canvasWidth / 2 - 64,
+    ballPos: [canvasWidth / 2 - 16, canvasHeight / 2 - 16]
+};
+const socket = new WebSocket('ws://localhost:8080');
+socket.addEventListener('open', (event) => {
+    
+});
+
+socket.addEventListener('message', (event) => {
+    const object = JSON.parse(event.data);
+    console.log(event.data);
+
+    gameState.player1YPos = object.player1YPos;
+    gameState.player2YPos = object.player2YPos;
+    gameState.ballPos = object.ballPos;
+});
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowUp') {
+        socket.send('-1');
+    } else if (event.key === 'ArrowDown') {
+        socket.send('1')
+    }
+});
+
 let canvas = document.getElementById('gameCanvas');
 let context = canvas.getContext('2d');
 
 context.fillStyle = 'green';
 context.fillRect(0, 0, 32, 600);
 context.fillRect(832, 0, 32, 600);
+
+const render = () => {
+    context.clearRect(32, 0, canvasWidth - 64, canvasHeight);
+    context.fillRect(64, gameState.player1YPos, 32, 128);
+    context.fillRect(canvasWidth - 3 * 32, gameState.player2YPos, 32, 128);
+    context.fillRect(gameState.ballPos[0], gameState.ballPos[1], 32, 32);
+};
+
+let frame = 1000 / 60;
+let loop = () => {
+    render();
+    setTimeout(loop, frame);
+};
+setTimeout(loop, frame);
